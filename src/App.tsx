38846ecb891d898e9619d8be54c29c31b35d2986
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-08 17:01:18 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { Header } from '~/components'
 
-const MAX_WIDTH = 1760
+// const MAX_WIDTH = 1760
 
 function App() {
   return (
-    <div
-      className="grid min-h-screen
-      bg-xgray-800
-      grid-rows-[[header_hero]_min-content_[header-end]_auto_[hero-end]]
-      grid-cols-[1fr_[content]_repeat(12,minmax(0,148px))_[content-end]_1fr] gap-x-2"
-    >
+    <div className="wrapper grid gap-x-2.5 bg-xgray-800">
       <Header />
     </div>
   )
--- src/components/Header/config.tsx
+++ src/components/Header/config.tsx
@@ -1,37 +1,5 @@
-import GithubIcon from '~/assets/github-icon.svg?react'
-import LinkedInIcon from '~/assets/linkedin-icon.svg?react'
-import TelegramIcon from '~/assets/telegram-icon.svg?react'
-import TwitterIcon from '~/assets/twitter-icon.svg?react'
-import cvIcon from '~/assets/cv-icon.svg?react'
-
 const config = {
-  links: [
-    {
-      Icon: GithubIcon,
-      name: 'Github',
-      repo: 'https://github.com/slapxxi',
-    },
-    {
-      Icon: TwitterIcon,
-      name: 'X',
-      repo: 'https://x.com/slapxxi',
-    },
-    {
-      Icon: LinkedInIcon,
-      name: 'LinkedIn',
-      repo: 'https://www.linkedin.com/in/%D0%B2%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2-%D0%BF%D0%B0%D0%B2%D0%BB%D1%83%D1%82%D0%B8%D0%BD-9417a2355/',
-    },
-    {
-      Icon: TelegramIcon,
-      name: 'Telegram',
-      repo: 'https://t.me/slapxxi',
-    },
-    {
-      Icon: cvIcon,
-      name: 'CV',
-      repo: 'https://slapxxi.github.io/cv/',
-    },
-  ],
+  nav: ['Equipment', 'About Us', 'Blog'],
 }
 
 export default config
