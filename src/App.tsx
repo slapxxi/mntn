e519a5f791612919c1347b9c85a1f4be8a22fe84
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-08 16:42:03 +0300

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { Header } from '~/components'
 
+const MAX_WIDTH = 1760
+
 function App() {
   return (
-    <div className="">
+    <div
+      className="grid min-h-screen
+      bg-xgray-800
+      grid-rows-[[header_hero]_min-content_[header-end]_auto_[hero-end]]
+      grid-cols-[1fr_[content]_repeat(12,minmax(0,148px))_[content-end]_1fr] gap-x-2"
+    >
       <Header />
     </div>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,22 +2,8 @@ import config from './config'
 
 export const Header = () => {
   return (
-    <header className="col-[content] py-4">
-      <ul className="flex gap-3 flex-wrap items-center justify-between max-w-prose mx-auto">
-        {config.links.map((link) => (
-          <li key={link.name}>
-            <a
-              href={link.repo}
-              rel="noreferrer"
-              target="_blank"
-              className="flex items-center gap-1 text-zinc-950/90 hover:text-blue-800/80 text-xs md:text-lg"
-            >
-              <link.Icon className="size-6" />
-              <span>{link.name}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
+    <header className="col-[content] row-[header] py-4 text-white text-fluid-lg">
+      header
     </header>
   )
 }
